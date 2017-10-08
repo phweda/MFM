462bb8a207c7f182aa845090b3539b6bc209ae76
Slithery <git@slithery.uk>
2017-10-09 00:15:19 +0100

    Improve MFM.sh with automatic architecture detection and set executable bit

--- MFM.sh
+++ MFM.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Command Switches: -list (list only view); -d (debug logging); -m (memory usage logging); -all (Includes non runnable systems)
-java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms2g -Xmx3g -jar ./MFM.jar
 
-# Next entry is for 32 bit Java if 64 bit is not possible
-#java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms1280m -Xmx1280m -jar ./MFM.jar
+if [[ $(uname -m) == 'x86_64' ]]; then
+  java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms2g -Xmx3g -jar ./MFM.jar
+else
+  java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms1280m -Xmx1280m -jar ./MFM.jar
+fi
 
 # Next entry is for full debug
 #java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xloggc:./Logs/MFM_GC_log.txt -Xms2g -Xmx3g -jar ./MFM.jar -d -s -m
