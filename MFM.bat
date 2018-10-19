9584153fa84d4b74c9747360eb63afa51e292d09
phweda <phweda1@yahoo.com>
2018-10-19 17:07:27 -0400

    Update batch and shell files for new memory usage (improved) Fixed .bat newlines for Mac

--- MFM.bat
+++ MFM.bat
@@ -1,13 +1,10 @@
 @echo off
-REM Command Switches: -list (list only view); -d (debug logging); -m (memory usage logging); -all (Includes non runnable systems)
+REM Command Switches: -list (list only view); -d (debug logging); -m (memory usage logging);
 
-java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms2g -Xmx3g -jar .\MFM.jar
+java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms1g -Xmx3g -jar .\MFM.jar
 
 REM Next entry is for 32 bit Java if 64 bit is not possible
 REM java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms1280m -Xmx1280m -jar .\MFM.jar
 
 REM Next entry is for full debug
-REM java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xloggc:./Logs/MFM_GC_log.txt -Xms2g -Xmx3g -jar .\MFM.jar -d -s -m
-
-REM Next entry is for running recent(MAME 143+) ALL sets
-REM java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms4g -Xmx4g -jar .\MFM.jar
+REM java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xloggc:./Logs/MFM_GC_log.txt -Xms1g -Xmx3g -jar .\MFM.jar -d -s -m
--- MFM.sh
+++ MFM.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# Command Switches: -list (list only view); -d (debug logging); -m (memory usage logging); -all (Includes non runnable systems)
+# Command Switches: -list (list only view); -d (debug logging); -m (memory usage logging);
 
 if [[ $(uname -m) == 'x86_64' ]]; then
-  java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms2g -Xmx3g -jar ./MFM.jar
+  java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms1g -Xmx3g -jar ./MFM.jar
 else
   java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms1280m -Xmx1280m -jar ./MFM.jar
 fi
 
 # Next entry is for full debug
-#java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xloggc:./Logs/MFM_GC_log.txt -Xms2g -Xmx3g -jar ./MFM.jar -d -s -m
-
-# Next entry is for running recent(MAME 143+) ALL sets
-# java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xms4g -Xmx4g -jar .\MFM.jar
+#java -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -Xloggc:./Logs/MFM_GC_log.txt -Xms1g -Xmx3g -jar ./MFM.jar -d -s -m
